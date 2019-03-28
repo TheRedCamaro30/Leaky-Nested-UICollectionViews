d2f68280ab1708f88e70543f965ee4c1422c7e2b
Tile <tiletechnologyllc@gmail.com>
2019-03-28 16:08:54 -0400

    changed addCell method in CellTappedDelegate

--- CollectionViewMemoryIssue/CellTappedDelegate.swift
+++ CollectionViewMemoryIssue/CellTappedDelegate.swift
@@ -18,9 +18,10 @@ extension MainViewController:CellTappedDelegate{
     func addCell() {
         print("cell tapped called")
         self.collectionView.performBatchUpdates({
-            self.collectionView.deleteItems(at: [IndexPath(item: self.arr.count - 2, section: 0), IndexPath(item: arr.count - 1, section: 0)])
+//            self.collectionView.deleteItems(at: [IndexPath(item: self.arr.count - 2, section: 0), IndexPath(item: arr.count - 1, section: 0)])
             arr.append(arr[arr.count - 1] + 1)
-            self.collectionView.insertItems(at: [IndexPath(item: arr.count -  3, section: 0),IndexPath(item: arr.count -  2, section: 0),IndexPath(item: arr.count - 1, section: 0)])
+            self.collectionView.insertItems(at: [IndexPath(item: arr.count - 1, section: 0)])
+//            self.collectionView.insertItems(at: [IndexPath(item: arr.count -  3, section: 0),IndexPath(item: arr.count -  2, section: 0),IndexPath(item: arr.count - 1, section: 0)])
         }) { (completed) in
             print("Perform batch updates completed")
             if completed == false{
